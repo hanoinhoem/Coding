fd0924038f12d8d1e49d83c621c9a66f9bf6e3d6
Hao Pham <phamnhathao95@gmail.com>
2025-07-20 21:06:16 +0700

    xoa file c

---
+++ math_utils.c
@@ -0,0 +1,9 @@
+#include "math_utils.h"
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
---
+++ math_utils.h
@@ -0,0 +1,10 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+// Cách 1: Không cần viết extern, vì mặc định đã là extern
+extern int sum(int a, int b);
+
+// Cách 2: Viết rõ extern (cũng được)
+extern int multiply(int a, int b);
+
+#endif
